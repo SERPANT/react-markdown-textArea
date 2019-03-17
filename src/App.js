6877cdd91ce3a89703bb2cceaea873ec22832905
Shreejit <shreejit955@gmail.com>
2019-03-17 22:23:38 +0545

    add basic markdown feature

--- src/App.js
+++ src/App.js
@@ -1,27 +1,69 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import marked from 'marked';
+
+import './app.css';
 
 class App extends Component {
-  render() {
+  constructor() {
+    super();
+    this.state = { isSelected: false, width: 256, height: 36, text: '' };
+    this.textAreaRef = React.createRef();
+    this.divTextAreaRef = React.createRef();
+  }
+
+  setSize = () => {
+    const { clientHeight, clientWidth } = this.textAreaRef.current;
+
+    this.setState({ width: clientWidth, height: clientHeight });
+  };
+
+  switchView = () => {
+    this.setState({ isSelected: !this.state.isSelected });
+  };
+
+  componentDidUpdate = () => {
+    if (!this.state.isSelected) {
+      this.divTextAreaRef.current.innerHTML = marked(this.state.text);
+    }
+  };
+
+  getView = () => {
+    const { isSelected, height, width } = this.state;
+
+    if (isSelected) {
+      return (
+        <textarea
+          className="text-area"
+          ref={this.textAreaRef}
+          value={this.state.text}
+          autoFocus="autoFocus"
+          onBlur={() => {
+            this.setSize();
+            this.switchView();
+          }}
+          onChange={e => {
+            this.setState({ text: e.target.value });
+          }}
+          style={{ width: `${width}px`, height: `${height}px` }}
+        />
+      );
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div
+        className="div-text-area"
+        ref={this.divTextAreaRef}
+        onClick={this.switchView}
+        style={{ width: `${width}px`, height: `${height}px` }}
+      />
     );
+  };
+
+  render() {
+    const currentView = this.getView();
+
+    return <div className="react-markdown-textarea">{currentView}</div>;
   }
 }
 
