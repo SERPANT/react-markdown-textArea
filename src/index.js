bcc0384be0e4c0a722b6bcf1f642ff3bcf4ed541
Shreejit <shreejit955@gmail.com>
2019-04-27 14:05:05 +0545

    remove test code

--- src/index.js
+++ src/index.js
@@ -3,7 +3,4 @@ import ReactDOM from 'react-dom';
 
 import MarkDownTextField from './MarkDownTextField';
 
-ReactDOM.render(
-  <MarkDownTextField placeHolder="hello hello " />,
-  document.getElementById('root')
-);
+ReactDOM.render(<MarkDownTextField />, document.getElementById('root'));
